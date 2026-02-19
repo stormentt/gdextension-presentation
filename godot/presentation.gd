ba3f933090afc10538a0b5eac5f58c596299523f
Tanner Storment <tannerstorment@gmail.com>
2026-02-18 20:51:24 -0600

    add fullscreen & exit

--- godot/presentation.gd
+++ godot/presentation.gd
@@ -5,3 +5,9 @@ func _input(event: InputEvent):
 		$PresentationInfo.next_slide()
 	if event.is_action_pressed("last_slide"):
 		$PresentationInfo.last_slide()
+	if event.is_action_pressed("fullscreen"):
+		var mode := DisplayServer.window_get_mode()
+		var is_window = mode != DisplayServer.WINDOW_MODE_FULLSCREEN
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN if is_window else DisplayServer.WINDOW_MODE_WINDOWED)
+	if event.is_action_pressed("exit"):
+		get_tree().quit(0)
